6bea0075a66719cca92058fb51f8384c990e1466
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-05-18 22:23:13 -0700

    Feat(xp): succesfully talking to (prod) webDB

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
+use futures::TryStreamExt;
 use sqlx::mysql::MySqlPool;
+use sqlx::Row;
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
-    let pool = MySqlPool::connect("mysql://root:root@127.0.0.1/student").await?;
+    // SDM gated US-prod_webDB
+    let pool = MySqlPool::connect("mysql://127.0.0.1:13309/pagerduty_production").await?;
+    // let pool = MySqlPool::connect("mysql://root:root@127.0.0.1/student").await?;
 
-    let row: (i32,) = sqlx::query_as("SELECT COUNT(*) FROM students")
-        .fetch_one(&pool)
-        .await?;
+    let mut rows = sqlx::query("SELECT subdomain FROM accounts LIMIT 3").fetch(&pool);
 
-    println!("Number of students: {}", row.0);
+    while let Some(row) = rows.try_next().await? {
+        // dbg!(row);
+        let subdomain: &str = row.try_get("subdomain")?;
+        println!("Subdomain, first three in __ order: {}", subdomain);
+    }
+
+    let mut rows = sqlx::query("SELECT subdomain FROM accounts WHERE id = 10 LIMIT 1").fetch(&pool);
+
+    while let Some(row) = rows.try_next().await? {
+        // dbg!(row);
+        let subdomain: &str = row.try_get("subdomain")?;
+        println!("Subdomain for id '10': {}", subdomain);
+    }
 
     Ok(())
 }

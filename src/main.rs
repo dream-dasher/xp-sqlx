55cfd677bfb5cca81e0b84cc659528523e8ac5e9
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-05-18 23:35:30 -0700

    Feat: elaborated database

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,27 @@ use sqlx::mysql::MySqlPool;
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
-    let pool = MySqlPool::connect("mysql://root:root@127.0.0.1/student").await?;
+    let pool = MySqlPool::connect("mysql://root:root@127.0.0.1/university").await?;
 
     let row: (i32,) = sqlx::query_as("SELECT COUNT(*) FROM students")
         .fetch_one(&pool)
         .await?;
-
     println!("Number of students: {}", row.0);
+
+    let row: (i32,) = sqlx::query_as("SELECT COUNT(*) FROM professors")
+        .fetch_one(&pool)
+        .await?;
+    println!("Number of professors: {}", row.0);
+
+    let row: (i32,) = sqlx::query_as("SELECT COUNT(*) FROM courses")
+        .fetch_one(&pool)
+        .await?;
+    println!("Number of courses: {}", row.0);
+
+    let row: (i32,) = sqlx::query_as("SELECT COUNT(*) FROM enrollments")
+        .fetch_one(&pool)
+        .await?;
+    println!("Number of enrollments: {}", row.0);
 
     Ok(())
 }

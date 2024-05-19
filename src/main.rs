f0982906c96c34d51b7a8520fbe2f84d266eab47
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-05-18 21:28:23 -0700

    Feat(xp): sqlx connecting to local db

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::mysql::MySqlPool;
+
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    let pool = MySqlPool::connect("mysql://root:root@127.0.0.1/student").await?;
+
+    let row: (i32,) = sqlx::query_as("SELECT COUNT(*) FROM students")
+        .fetch_one(&pool)
+        .await?;
+
+    println!("Number of students: {}", row.0);
+
+    Ok(())
 }

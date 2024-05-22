dd12623fa834090b7d913e550256eeb078133860
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-05-22 11:37:12 -0700

    Feat: query param input via clap

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
+use clap::Parser;
 use futures::TryStreamExt;
 use sqlx::mysql::MySqlPool;
 use sqlx::Row;
 
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    // like element to search for in subdomain names
+    likeness: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
+    let args = Args::parse();
+
     // SDM gated US-prod_webDB
     let pool = MySqlPool::connect("mysql://127.0.0.1:13309/pagerduty_production").await?;
     // let pool = MySqlPool::connect("mysql://root:root@127.0.0.1/student").await?;
 
     let mut rows = sqlx::query("SELECT subdomain FROM accounts WHERE subdomain LIKE ? LIMIT 3")
-        .bind("%pdt%")
+        .bind(format!("%{}%", args.likeness))
         .fetch(&pool);
 
     while let Some(row) = rows.try_next().await? {

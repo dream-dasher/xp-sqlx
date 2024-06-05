c2d6610c8df7282540c1d2715f7cbd96df911c86
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-06-05 13:42:50 -0700

    Feat: CLI wrapping implementations for benching

--- src/bin/transpose_implementations.rs
+++ src/bin/transpose_implementations.rs
@@ -1,10 +1,42 @@
 //! comparing (running) come different ways implementations that
 //! get a sqlx stream into a polars::DataFrame
 
+use derive_more::{Constructor, Display};
 use std::time::Instant;
 use xp_sqlx::stream_to_df::{direct_transpose, recopy_transpose, vstruct_transpose};
 
-#[derive(Debug)]
+use clap::{Parser, ValueEnum};
+#[derive(Parser, Debug)]
+#[command(version, about)]
+/// Arguments to select MemoryTranspose Implementations and Repetition of DB draws (increasing data transposed)
+/// Principally for use with Hyperfine to do benchmarking.
+/// (Preferred benchmarking framework (Divan) does not currently support async operations.)
+///
+/// Note: This requires an active DataBase on a specific port.
+/// Justfile in the associatted repo has docker create and destroy code to set one up.
+/// `template.env` has default address for the dockerized DB
+struct Args {
+    /// Select the implementation to test
+    implementation: TransImpl,
+    /// Number of times to repeat the test
+    repetition: u32,
+}
+
+#[derive(ValueEnum, Clone, Debug)]
+enum TransImpl {
+    Direct,
+    Recopy,
+    VStruct,
+    All,
+}
+
+#[derive(Debug, Constructor, Display)]
+#[display(
+    fmt = "Direct impl time: {}\nRecopy impl time: {}\nVStruct impl time: {}",
+    direct,
+    recopy,
+    vstruct
+)]
 struct TimesTaken {
     pub direct: u128,
     pub vstruct: u128,
@@ -13,27 +45,42 @@ struct TimesTaken {
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
-    let mut times_taken = TimesTaken {
-        direct: 0,
-        vstruct: 0,
-        recopy: 0,
+    let args = Args::parse();
+    let reps = args.repetition;
+
+    let now = Instant::now();
+    match args.implementation {
+        TransImpl::Direct => direct_transpose(reps).await?,
+
+        TransImpl::VStruct => recopy_transpose(reps).await?,
+
+        TransImpl::Recopy => vstruct_transpose(reps).await?,
+        TransImpl::All => {
+            let mut elapsed_times_struct = TimesTaken::new(0, 0, 0);
+
+            let now = Instant::now();
+            direct_transpose(reps).await?;
+            let elapsed_time = now.elapsed();
+            elapsed_times_struct.direct = elapsed_time.as_millis();
+
+            let now = Instant::now();
+            recopy_transpose(reps).await?;
+            let elapsed_time = now.elapsed();
+            elapsed_times_struct.recopy = elapsed_time.as_millis();
+
+            let now = Instant::now();
+            vstruct_transpose(reps).await?;
+            let elapsed_time = now.elapsed();
+            elapsed_times_struct.vstruct = elapsed_time.as_millis();
+
+            println!("\n\nTimes Elapsed: {}", elapsed_times_struct);
+        }
     };
 
-    let now = Instant::now();
-    direct_transpose(20_000).await?;
     let elapsed_time = now.elapsed();
-    times_taken.direct = elapsed_time.as_millis();
-
-    let now = Instant::now();
-    recopy_transpose(20_000).await?;
-    let elapsed_time = now.elapsed();
-    times_taken.recopy = elapsed_time.as_millis();
-
-    let now = Instant::now();
-    vstruct_transpose(20_000).await?;
-    let elapsed_time = now.elapsed();
-    times_taken.vstruct = elapsed_time.as_millis();
-
-    println!("\n\nTimes Recorded (ms):\n{:#?}", times_taken);
+    println!(
+        "\n\nTotal Time Recorded (ms):\n{:#?}",
+        elapsed_time.as_millis()
+    );
     Ok(())
 }

296ef2121d0f06ce42462b9f8a029b79d137f644
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2025-01-08 14:25:40 -0500

    feat: subcommand based access (and clearer intent)

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,78 @@
 use std::fmt::Debug;
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use futures::TryStreamExt;
-use sqlx::mysql::MySqlPool;
-use sqlx::Row;
+use sqlx::{Row, mysql::MySqlPool};
 
+/// Simple query on subdomains
 #[derive(Parser, Debug)]
 #[command(version, about)]
 struct Args {
-    // Regex string to compare to `account.subdomain`
-    regex: String,
+        // Regex string to compare to `account.subdomain`
+        #[command(subcommand)]
+        command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+        /// Search subdomain names using regex
+        SubdomainSearch {
+                ///Regex pattern to match against subdomain names
+                regex: String,
+        },
 }
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
-    let args = Args::parse();
+        let args = Args::parse();
 
-    // SDM gated US-prod_webDB
-    let pool_us = MySqlPool::connect("mysql://127.0.0.1:13309/pagerduty_production").await?;
-    // SDM gated EU-prod_webDB
-    let pool_eu = MySqlPool::connect("mysql://127.0.0.1:13310/pagerduty_production").await?;
-    for (pool, region_str) in [(pool_us, "US"), (pool_eu, "EU")] {
-        println!("\nPool corresponding to region: {:?}", region_str);
-        let mut rows_dyn_fetch =
-            sqlx::query("SELECT subdomain FROM accounts WHERE subdomain REGEXP ? LIMIT 3")
-                .bind(format!("{}", args.regex))
-                .fetch(&pool);
+        // SDM gated US-prod_webDB
+        let pool_us = MySqlPool::connect("mysql://127.0.0.1:13309/pagerduty_production").await?;
+        // SDM gated EU-prod_webDB
+        let pool_eu = MySqlPool::connect("mysql://127.0.0.1:13310/pagerduty_production").await?;
 
-        while let Some(row) = rows_dyn_fetch.try_next().await? {
-            // dbg!(row);
-            let subdomain: &str = row.try_get("subdomain")?;
-            println!(
-                "Dyn fetch: Subdomain, first three in __ order: {}",
-                subdomain
-            );
+        match &args.command {
+                Commands::SubdomainSearch { regex } => {
+                        for (pool, region_str) in [(pool_us, "US"), (pool_eu, "EU")] {
+                                println!("\nPool corresponding to region: {:?}", region_str);
+                                let mut rows_dyn_fetch =
+                                        sqlx::query("SELECT subdomain FROM accounts WHERE subdomain REGEXP ? LIMIT 3")
+                                                .bind(regex)
+                                                .fetch(&pool);
+
+                                while let Some(row) = rows_dyn_fetch.try_next().await? {
+                                        // dbg!(row);
+                                        let subdomain: &str = row.try_get("subdomain")?;
+                                        println!("Dyn fetch: Subdomain, first three in __ order: {}", subdomain);
+                                }
+
+                                // NOTE: verified (macro) queries take param values as *arguments* rather than bind methods.
+                                let mut rows_stat_fetch = sqlx::query!(
+                                        "SELECT subdomain FROM accounts WHERE subdomain REGEXP ? LIMIT 3",
+                                        format!("{}", regex)
+                                )
+                                .fetch(&pool);
+
+                                while let Some(row) = rows_stat_fetch.try_next().await? {
+                                        // dbg!(row);
+                                        let subdomain = row.subdomain;
+                                        println!(
+                                                "Static check fetch: Subdomain, first three in __ order: {}",
+                                                subdomain
+                                        );
+                                }
+
+                                let mut rows = sqlx::query("SELECT subdomain FROM accounts WHERE id = 10 LIMIT 1")
+                                        .fetch(&pool);
+
+                                while let Some(row) = rows.try_next().await? {
+                                        // dbg!(row);
+                                        let subdomain: &str = row.try_get("subdomain")?;
+                                        println!("Subdomain for id '10': {}", subdomain);
+                                }
+                        }
+                }
         }
 
-        // NOTE: verified (macro) queries take param values as *arguments* rather than bind methods.
-        let mut rows_stat_fetch = sqlx::query!(
-            "SELECT subdomain FROM accounts WHERE subdomain REGEXP ? LIMIT 3",
-            format!("{}", args.regex)
-        )
-        .fetch(&pool);
-
-        while let Some(row) = rows_stat_fetch.try_next().await? {
-            // dbg!(row);
-            let subdomain = row.subdomain;
-            println!(
-                "Static check fetch: Subdomain, first three in __ order: {}",
-                subdomain
-            );
-        }
-
-        let mut rows =
-            sqlx::query("SELECT subdomain FROM accounts WHERE id = 10 LIMIT 1").fetch(&pool);
-
-        while let Some(row) = rows.try_next().await? {
-            // dbg!(row);
-            let subdomain: &str = row.try_get("subdomain")?;
-            println!("Subdomain for id '10': {}", subdomain);
-        }
-    }
-
-    Ok(())
+        Ok(())
 }
